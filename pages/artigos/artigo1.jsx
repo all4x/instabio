42cf8fc06d064e139cda7432e6d73cda42680b7b
all3x <estudar0101@gmail.com>
2021-10-29 18:34:26 +0100

    'esborso'

---
+++ pages/artigos/artigo1.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Main from './src/main'
+
+export default function Home() {
+    return(
+        <div>
+            <Head>
+                
+            </Head>
+            <main>
+                <Main></Main>
+            </main>
+        </div>
+    )
+}
