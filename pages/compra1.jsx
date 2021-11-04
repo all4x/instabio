cb07628c40810f337f02ea9189d5b2b5bd5e2809
all3x <estudar0101@gmail.com>
2021-11-04 15:47:51 -0300

    migração braip

--- pages/compra1.jsx
+++ pages/compra1.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     return(
         <div>
         <Head>
-            <meta http-equiv="refresh" content="2;https://app.monetizze.com.br/r/AFY17713867?u=c&pl=CA105279&src=paginadevendas" />
+            <meta http-equiv="refresh" content="2;https://ev.braip.com/ref?pl=pla962yy&ck=chew8wl8&af=afi3yz2z9" />
         </Head> 
         <div className=' flex flex-col items-center justify-center min-h-screen text-4xl font-light animate-pulse'>
         <h1>Carregando</h1>
--- pages/compra4.jsx
+++ pages/compra4.jsx
@@ -1,15 +1,14 @@
 import Head from 'next/head'
-import Pixel from '../src/components/Pixel'
 
 export default function Home() {
     return(
         <div>
         <Head>
-            <meta http-equiv="refresh" content="2;https://app.monetizze.com.br/r/AFY17713867?u=c&pl=AL99177&src=paginadevendas4" />
+            <meta http-equiv="refresh" content="2;https://ev.braip.com/ref?pl=plaqq68x&ck=chew8wl8&af=afi3yz2z9" />
         </Head> 
         <div className=' flex flex-col items-center justify-center min-h-screen text-4xl font-light animate-pulse'>
         <h1>Carregando</h1>
-        <Pixel name='FACEBOOK_PIXEL_1' />
+      
         </div>
 
         </div>
