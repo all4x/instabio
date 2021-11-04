ea66160211a549998c8e75a119a89c89391a9644
all3x <estudar0101@gmail.com>
2021-11-04 18:13:33 -0300

    voltei

--- pages/compra1.jsx
+++ pages/compra1.jsx
@@ -1,15 +1,15 @@
 import Head from 'next/head'
-import Pixel from '../src/components/Pixel'
+
 
 export default function Home() {
     return(
         <div>
         <Head>
-            <meta http-equiv="refresh" content="2;https://ev.braip.com/ref?pl=pla962yy&ck=chew8wl8&af=afi3yz2z9" />
+            <meta http-equiv="refresh" content="2;https://app.monetizze.com.br/r/AFY17713867?u=c&pl=CA105279" />
         </Head> 
         <div className=' flex flex-col items-center justify-center min-h-screen text-4xl font-light animate-pulse'>
         <h1>Carregando</h1>
-        <Pixel name='FACEBOOK_PIXEL_1' />
+     
         </div>
 
         </div>
