d50b861ad23ca89634acc0f4f3c183bb91c10aa0
all3x <estudar0101@gmail.com>
2021-11-05 17:12:21 -0300

    'google-ads'

--- pages/liftdetox2.jsx
+++ pages/liftdetox2.jsx
@@ -9,20 +9,18 @@ import Botaozap from "../src/components/Botaozap";
 
 export default function Home() {
     return(
-        <div className="bg-gray-300">
+        <div className="bg-gray-300 px-2">
         <Head>
         <title>Nova Forma De Fazer Dieta</title>
         <meta property="og:title"           content="DietaVirtual" /> 
         <meta property="og:type"            content="article" /> 
-        <meta property="og:url"             content="www.br116na-madruga.ga/lifitdetox2" /> 
-        <meta content='www.br116na-madruga.ga/liftdetox2' ng-if='og_meta.url' property='og:url'/>
+        <meta property="og:url"             content="www.nyldecosmeticos.ga/lifitdetox2" /> 
+
         <link rel="icon" href="/detox.svg" />
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <meta name="author" content="Nylde"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-        <meta property="og:description" content="A melhor forma de perde peso"/> 
-        <meta name="facebook-domain-verification" content="lttx508tcn1mshd90ranof9c7a422l" />   
-        <meta property="og:image"  content="https://www.br116na-madruga.ga/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fdepo1.58deb121bd4552dfa44ffd1a021b0a5d.jpeg&w=384&q=75" />
+        <meta property="og:description" content="A melhor forma de perde peso"/>  
         </Head>
         <Main></Main>
         <Quadro></Quadro>
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -5,8 +5,8 @@ import Image from 'next/image'
 const Botao = () => {
     return(
         <div className="">
-            <div className="p-4">
-                <h1 className="text-5xl p-5 text-center font-black bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-blue-900">Definitivamente Magra</h1>
+            <div className="">
+                <h1 className="text-4xl py-4 text-center font-black bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-blue-900">Definitivamente Magra</h1>
                 <div className="bg-gray-200 py-2 border-b-4 rounded-2xl shadow-xl">
                     <h2 className="text-center font-bold text-xl">Programa de Reeducação Alimentar Ajuda <span className="text-green-500"> Mais de 10.000 Mulheres nos Últimos 2 Anos</span> que Precisavam Reduzir Drasticamente Suas GORDURAS CORPORAIS em poucos dias!</h2>
                 </div>
