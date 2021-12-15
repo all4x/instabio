6e4485635ffcf634059f0cd1fcb0694429abad05
all3x <estudar0101@gmail.com>
2021-12-15 11:18:51 -0300

    natal

--- pages/promocao.jsx
+++ pages/promocao.jsx
@@ -5,13 +5,13 @@ export default function Home() {
     return(
         <div>
         <Head>
-          <meta name="title" content="Nylde Cosmeticos | Black Friday"/>
+          <meta name="title" content="Nylde Cosmeticos | Promoção de Natal"/>
           <link rel="icon" href="/detox.png" />
           <meta name="description" content="Lift Detox Caps possui fórmula 100% natural que contém ingredientes potentes capazes de agilizar o processo de emagrecimento, sem precisar sair de casa para academia"/>
           <meta http-equiv="refresh" content="2;https://app.monetizze.com.br/r/AFY17713867/?u=RZ64248" />
         </Head> 
-        <div className=' flex flex-col items-center justify-center min-h-screen text-4xl font-light animate-pulse'>
-        <h1>Carregando</h1>
+        <div className=' flex flex-col items-center justify-center min-h-screen text-4xl font-light animate-pulse text-center'>
+        <h1>Aplicando Cupom de Desconto!</h1>
      
         </div>
 
