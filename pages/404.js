d15db161866a4940dad1dd0e6d05a5b5d28860f8
all3x <estudar0101@gmail.com>
2021-12-28 16:55:32 -0300

    alou

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,14 @@
 // pages/404.js
 export default function Custom404() {
     return (
-        <div className="flex justify-center">
-            <div><h1 className="text-center p-10 text-2xl font-sans">404 - Está pagina não existe mais 🙁</h1></div>
+        <div className="flex-col justify-center">
+            <div>
+                <h1 className="text-center p-10 text-xl font-sans text-gray-500">404 - Está promoção não está mais ativa🙁, porfavor entre em contato </h1>
+                </div>
+            <div className=" text-4xl bg-opacity-25 bg-white text-black rounded-full p-2">
+                <div  className="text-center">
+                    <a href="https://api.whatsapp.com/send?phone=5563992072188&"><button class="bg-yellow-200  hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full w-80">Whatsapp</button></a>
+            </div> </div>       
         </div>
     )
 }
