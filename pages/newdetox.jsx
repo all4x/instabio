4c945595fd6d32ad458e652625b65e40ff7e1d09
all3x <estudar0101@gmail.com>
2022-01-14 10:29:10 -0300

    new page

--- pages/newdetox.jsx
+++ pages/newdetox.jsx
@@ -15,12 +15,12 @@ export default function Home() {
         <div>
            
         {/* Button */}
-            <div class="mt-4 animate-pulse">
+            <div class="mt-4">
             <a href='https://app.monetizze.com.br/r/AFN18833790?src=PaginaDeVendasLouja'>
                 <button type="button" class='flex max-w-sm w-full bg-gradient-to-r from-green-500 to-blue-500 hover:from-green-600 hover:to-blue-600 focus:outline-none text-white text-3xl uppercase font-bold shadow-md mx-auto p-5'>
-                    <div class="flex sm:flex-cols-12 gap-6">
+                    <div class="flex sm:flex-cols-12 gap-1">
                         <div class="col-span-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
                             </svg>
                         </div>
