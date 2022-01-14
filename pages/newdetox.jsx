3b7b2c5baa49880c4c9f10eb9ded38a611497d71
all3x <estudar0101@gmail.com>
2022-01-14 10:21:57 -0300

    new page

--- pages/newdetox.jsx
+++ pages/newdetox.jsx
@@ -8,10 +8,37 @@ export default function Home() {
           <meta name="title" content="Nylde Cosmeticos | New Detox"/>
           <link rel="icon" href="/detox.png" />
           <meta name="description" content="New Detox possui fórmula 100% natural que contém ingredientes potentes capazes de agilizar o processo de emagrecimento, sem precisar sair de casa para academia"/>
-          <meta http-equiv="refresh" content="0;https://app.monetizze.com.br/r/AFN18833790?src=PaginaDeVendasLouja" />
         </Head> 
-        <div className='m-3 flex flex-col items-center justify-center min-h-screen text-3xl font-light animate-pulse text-center'>
-        <h1>Direcionando para a página solicitada.</h1>
+        <div className='m-3 flex flex-col items-center justify-center min-h-screen text-3xl font-light text-center'>
+        <h1 className='font-bold text-gray-500'>Tudo certo!</h1>
+        <h2 className='text-xl'>Clique em continuar para ser direcionado para a página solicitada.</h2>
+        <div>
+           
+        {/* Button */}
+            <div class="mt-4 animate-pulse">
+            <a href='https://app.monetizze.com.br/r/AFN18833790?src=PaginaDeVendasLouja'>
+                <button type="button" class='flex max-w-sm w-full bg-gradient-to-r from-green-500 to-blue-500 hover:from-green-600 hover:to-blue-600 focus:outline-none text-white text-3xl uppercase font-bold shadow-md mx-auto p-5'>
+                    <div class="flex sm:flex-cols-12 gap-6">
+                        <div class="col-span-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z" />
+                            </svg>
+                        </div>
+                        <h1>Continuar</h1>
+                        <div class="col-span-2 pt-1.5"></div>
+                    </div>    
+                </button>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div>
+            <h2 class="text-2xl mb-4 text-gray-700 font-bold"></h2>
+            <hr class="bg-gray-500 w-full h-0.25 mb-5"/>
+        {/* Button */}
+        
+        </div>
         </div>
        
         </div>
