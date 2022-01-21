aa45531fc043ec0486c20a842ae20b106bfe2ff1
all3x <estudar0101@gmail.com>
2022-01-21 02:23:25 -0300

    se der erro, fodeu-2 :)

--- pages/queda-capilar/index.js
+++ pages/queda-capilar/index.js
@@ -2,7 +2,7 @@ import Topbar from '/components/Topbar'
 import Hair from './newhair.jsx'
 import Cta from './cta.jsx'
 import Footer from '/components/Footer'
-import Nugget from './Nugget/index.js'
+import Nugget from './nugget/index.js'
 
 
 export default function Home() {
