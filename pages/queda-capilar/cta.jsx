a322add7e3e4ee3a7b071453f5a447095522251f
all3x <estudar0101@gmail.com>
2022-01-23 14:21:44 -0300

    até amanha amigo

--- pages/queda-capilar/cta.jsx
+++ pages/queda-capilar/cta.jsx
@@ -3,23 +3,43 @@
 
 import React from 'react';
 import Head from 'next/head';
-import Lead from '/pages/queda-capilar/lead'
-
+import Lead from './Lead'
+import Script from 'next/script';
 
 function cta() {
   return <div>
       <Head>
-            <Lead/>
+      <Script>
+                {
+                function gtag_report_conversion(url) {
+                  var callback = function () {
+                    if (typeof(url) != 'undefined') {
+                      window.location = url;
+                    }
+                  };
+                  gtag('event', 'conversion', {
+                      'send_to': 'AW-447631009/X5S5CLvg9pIDEKGdudUB',
+                      'value': 1.0,
+                      'currency': 'BRL',
+                      'event_callback': callback
+                  });
+                  return false;
+                }
+               
+                }
+        </Script>
       </Head>
       <h1 className=' text-center text-2xl font-black animate-pulse p-4'>Peça o seu! direto no Whatsapp👇</h1>
       {/*whatsapp*/}
       <div className=" text-4xl bg-opacity-25  text-black rounded-full p-2">
         
                 <div  className="text-center">
-                  {<Lead/>}
-                  <a onClick={
-                    <Lead/>
-                  } href="https://www.nyldecosmeticos.com.br/queda-capilar/whatsapp/button-1">
+                  
+                  <a href="https://www.nyldecosmeticos.com.br/queda-capilar/whatsapp/button-1" onClick={
+                   function gtag_report_conversion(url) {
+                     gtag_report_conversion(url)
+                   }
+                  }>
                   
                   <button class="p-2 pl-16 pr-16 bg-green-500 text-gray-100 text-lg rounded-lg focus:border-4 border-green-300">Whatsapp</button>
                   </a>
--- pages/queda-capilar/lead/index.js
+++ pages/queda-capilar/lead/index.js
@@ -2,24 +2,27 @@
 import Script from 'next/script';
 import React from 'react';
 
+
+
 function Lead() {
   return (
          <Script>
                 {
                 function gtag_report_conversion(url) {
-                var callback = function () {
+                  var callback = function () {
                     if (typeof(url) != 'undefined') {
-                    window.location = url;
+                      window.location = url;
                     }
-                };
-                gtag('event', 'conversion', {
-                    'send_to': 'AW-447631009/X5S5CLvg9pIDEKGdudUB',
-                    'value': 1.0,
-                    'currency': 'BRL',
-                    'event_callback': callback
-                });
-                return false;
+                  };
+                  gtag('event', 'conversion', {
+                      'send_to': 'AW-447631009/X5S5CLvg9pIDEKGdudUB',
+                      'value': 1.0,
+                      'currency': 'BRL',
+                      'event_callback': callback
+                  });
+                  return false;
                 }
+               
                 }
         </Script>
   )
