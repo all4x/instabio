fb88abe52b48f00e424ee5bdea590cf3c4df0550
all3x <estudar0101@gmail.com>
2022-01-23 13:55:23 -0300

    deu certo

--- pages/queda-capilar/cta.jsx
+++ pages/queda-capilar/cta.jsx
@@ -3,39 +3,22 @@
 
 import React from 'react';
 import Head from 'next/head';
-import Script from 'next/script'
+import Lead from '/pages/queda-capilar/lead'
 
 
 function cta() {
   return <div>
       <Head>
-            <Script>
-              {
-              function gtag_report_conversion(url) {
-                var callback = function () {
-                  if (typeof(url) != 'undefined') {
-                    window.location = url;
-                  }
-                };
-                gtag('event', 'conversion', {
-                    'send_to': 'AW-447631009/X5S5CLvg9pIDEKGdudUB',
-                    'value': 1.0,
-                    'currency': 'BRL',
-                    'event_callback': callback
-                });
-                return false;
-              }
-              }
-      
-      </Script>
+            <Lead/>
       </Head>
       <h1 className=' text-center text-2xl font-black animate-pulse p-4'>Peça o seu! direto no Whatsapp👇</h1>
       {/*whatsapp*/}
       <div className=" text-4xl bg-opacity-25  text-black rounded-full p-2">
         
                 <div  className="text-center">
+                  {<Lead/>}
                   <a onClick={
-                    gta_report_conversion(url)
+                    <Lead/>
                   } href="https://www.nyldecosmeticos.com.br/queda-capilar/whatsapp/button-1">
                   
                   <button class="p-2 pl-16 pr-16 bg-green-500 text-gray-100 text-lg rounded-lg focus:border-4 border-green-300">Whatsapp</button>
