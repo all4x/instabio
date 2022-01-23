5389475426fde5608500a0ccb01c2f178f4a05cd
all3x <estudar0101@gmail.com>
2022-01-23 15:01:11 -0300

    vou dormi fodase-3

--- pages/queda-capilar/whatsapp/button-1.js
+++ pages/queda-capilar/whatsapp/button-1.js
@@ -8,14 +8,14 @@ function index() {
   return (
         <div>
             <Head>
-            <meta name="title" content="Nylde Cosmeticos | Whatsapp"/>     
+            <meta name="title" content="Nylde Cosmeticos | Whatsapp"/>
             <meta http-equiv="refresh" content="0;https://api.whatsapp.com/send?phone=5563992072188&text=Ola%2C%20gostaria%20de%20realizar%20a%20compra%20do%20meu%20NEW%20HAIR!" />
             <Script>
               {
-              event('event', 'conversion', {'send_to': 'AW-447631009/V2jUCP7T75IDEKGdudUB'})                
-              }    
+              event('event', 'conversion', {'send_to': 'AW-447631009/V2jUCP7T75IDEKGdudUB'})
+              }
             </Script>
-            </Head> 
+            </Head>
                 <div className='m-3 flex flex-col items-center justify-center min-h-screen text-3xl font-light text-center'>
                 <h1 className='font-bold text-gray-500'>Tudo certo!</h1>
                 <h2 className='text-xl'>Fique nesta pagina para ser direcionado para o whatsapp.</h2>
@@ -25,6 +25,4 @@ function index() {
 }
 
 export default index;
-
-
-//            
+          
