1030ea8779c7ffd8c48c6444c6aa523805180aab
all3x <estudar0101@gmail.com>
2022-01-23 12:36:06 -0300

    tudo

--- pages/queda-capilar/cta.jsx
+++ pages/queda-capilar/cta.jsx
@@ -2,6 +2,7 @@
 
 
 import React from 'react';
+import Link from 'next/link'
 
 function cta() {
   return <div>
@@ -11,7 +12,26 @@ function cta() {
       <div className=" text-4xl bg-opacity-25  text-black rounded-full p-2">
         
                 <div  className="text-center">
-                    <a href="https://api.whatsapp.com/send?phone=5563992072188&text=Ola%2C%20gostaria%20de%20realizar%20a%20compra%20do%20meu%20NEW%20HAIR!"><button class="p-2 pl-16 pr-16 bg-green-500 text-gray-100 text-lg rounded-lg focus:border-4 border-green-300">Whatsapp</button></a>
+                  <Link href="/queda-capilar/whatsapp/button-1" onClick= {
+                    function gtag_report_conversion(url) {
+                      var callback = function () {
+                        if (typeof(url) != 'undefined') {
+                          window.location = url;
+                        }
+                      };
+                      gtag('event', 'conversion', {
+                          'send_to': 'AW-447631009/X5S5CLvg9pIDEKGdudUB',
+                          'value': 1.0,
+                          'currency': 'BRL',
+                          'event_callback': callback
+                      });
+                      return false;
+                    }
+                  }>
+                  
+                  <button class="p-2 pl-16 pr-16 bg-green-500 text-gray-100 text-lg rounded-lg focus:border-4 border-green-300">Whatsapp</button>
+                  </Link>
+                  
             </div>
                 </div>
 
