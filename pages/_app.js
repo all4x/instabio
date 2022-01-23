e3ad7e96219b73e1c842c167b7b80d9cc669aa58
all3x <estudar0101@gmail.com>
2022-01-23 10:50:25 -0300

    google ads

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
 import 'tailwindcss/tailwind.css'
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as gtag from 'lib/gtag'
+import Analytics from 'components/Analytics'
+
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+
+
+function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
+}
+
+export default App
