be70defcae7ec16a05acf244192cc296dc6f4735
all3x <estudar0101@gmail.com>
2022-01-23 14:50:14 -0300

    até amanha amigo-6

--- pages/queda-capilar/cta.jsx
+++ pages/queda-capilar/cta.jsx
@@ -4,27 +4,12 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link'
-import Script from 'next/script';
+
 
 function cta() {
   return <div>
       <Head>
-      <Script>
-                {
-                function gtag_report_conversion(url) {
-                  var callback = function () {
-                    if (typeof(url) != 'undefined') {
-                      window.location = url;
-                    }
-                  };
-                  gtag('event', 'conversion', {
-                      'send_to': 'AW-447631009/V2jUCP7T75IDEKGdudUB',
-                      'event_callback': callback
-                  });
-                  return false;
-                }  
-                }
-        </Script>
+      
       </Head>
       <h1 className=' text-center text-2xl font-black animate-pulse p-4'>Peça o seu! direto no Whatsapp👇</h1>
       {/*whatsapp*/}
@@ -32,18 +17,7 @@ function cta() {
         
                 <div  className="text-center">
                   
-                  <Link href="https://www.nyldecosmeticos.com.br/queda-capilar/whatsapp/button-1" onClick={function gtag_report_conversion(url) {
-                  var callback = function () {
-                    if (typeof(url) != 'undefined') {
-                      window.location = url;
-                    }
-                  };
-                  gtag('event', 'conversion', {
-                      'send_to': 'AW-447631009/V2jUCP7T75IDEKGdudUB',
-                      'event_callback': callback
-                  });
-                  return false;
-                }}>
+                  <Link href="https://www.nyldecosmeticos.com.br/queda-capilar/whatsapp/button-1" >
                   
                   <button class="p-2 pl-16 pr-16 bg-green-500 text-gray-100 text-lg rounded-lg focus:border-4 border-green-300">Whatsapp</button>
                   </Link>
--- pages/queda-capilar/whatsapp/button-1.js
+++ pages/queda-capilar/whatsapp/button-1.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import Head from 'next/head';
-
+import Script from 'next/script' 
 
 
 function index() {
@@ -10,6 +10,13 @@ function index() {
             <Head>
             <meta name="title" content="Nylde Cosmeticos | Whatsapp"/>     
             <meta http-equiv="refresh" content="0;https://api.whatsapp.com/send?phone=5563992072188&text=Ola%2C%20gostaria%20de%20realizar%20a%20compra%20do%20meu%20NEW%20HAIR!" />
+            <Script>
+              {
+                  <Script>
+              gtag('event', 'conversion', {'send_to': 'AW-447631009/V2jUCP7T75IDEKGdudUB'});
+                  </Script>
+              }    
+            </Script>
             </Head> 
                 <div className='m-3 flex flex-col items-center justify-center min-h-screen text-3xl font-light text-center'>
                 <h1 className='font-bold text-gray-500'>Tudo certo!</h1>
