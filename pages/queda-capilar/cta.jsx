34a203c6f01e7adffb63bd49131b5b6680176348
all3x <estudar0101@gmail.com>
2022-01-23 12:47:49 -0300

    tudo-1

--- pages/queda-capilar/cta.jsx
+++ pages/queda-capilar/cta.jsx
@@ -2,7 +2,7 @@
 
 
 import React from 'react';
-import Link from 'next/link'
+
 
 function cta() {
   return <div>
@@ -12,7 +12,7 @@ function cta() {
       <div className=" text-4xl bg-opacity-25  text-black rounded-full p-2">
         
                 <div  className="text-center">
-                  <Link href="/queda-capilar/whatsapp/button-1" onClick= {
+                  <a href="/queda-capilar/whatsapp/button-1" onClick= {
                     function gtag_report_conversion(url) {
                       var callback = function () {
                         if (typeof(url) != 'undefined') {
@@ -30,7 +30,7 @@ function cta() {
                   }>
                   
                   <button class="p-2 pl-16 pr-16 bg-green-500 text-gray-100 text-lg rounded-lg focus:border-4 border-green-300">Whatsapp</button>
-                  </Link>
+                  </a>
                   
             </div>
                 </div>
