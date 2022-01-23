f6b46550ab58b5626cbe994fbcb9f03ea74dadde
all3x <estudar0101@gmail.com>
2022-01-23 13:07:28 -0300

    test-2

--- pages/queda-capilar/cta.jsx
+++ pages/queda-capilar/cta.jsx
@@ -2,7 +2,7 @@
 
 
 import React from 'react';
-
+import Link from 'next/link'
 
 function cta() {
   return <div>
@@ -12,26 +12,10 @@ function cta() {
       <div className=" text-4xl bg-opacity-25  text-black rounded-full p-2">
         
                 <div  className="text-center">
-                  <a  onClick= {
-                    function gtag_report_conversion(url) {
-                      var url = 'https://www.nyldecosmeticos.com.br/queda-capilar/whatsapp/button-1'
-                      var callback = function () {
-                        if (typeof(url) != 'undefined') {
-                          window.location = url;
-                        }
-                      };
-                      gtag('event', 'conversion', {
-                          'send_to': 'AW-447631009/X5S5CLvg9pIDEKGdudUB',
-                          'value': 1.0,
-                          'currency': 'BRL',
-                          'event_callback': callback
-                      });
-                      return false;
-                    }
-                  } >
+                  <Link href="/queda-capilar/whatsapp">
                   
                   <button class="p-2 pl-16 pr-16 bg-green-500 text-gray-100 text-lg rounded-lg focus:border-4 border-green-300">Whatsapp</button>
-                  </a>
+                  </Link>
                   
             </div>
                 </div>
--- pages/queda-capilar/whatsapp/button-1.js
+++ pages/queda-capilar/whatsapp/button-1.js
@@ -8,8 +8,8 @@ function index() {
   return (
         <div>
             <Head>
+              
             <meta name="title" content="Nylde Cosmeticos | Whatsapp"/>
-            <meta http-equiv="refresh" content="2;https://api.whatsapp.com/send?phone=5563992072188&text=Ola%2C%20gostaria%20de%20realizar%20a%20compra%20do%20meu%20NEW%20HAIR!" />
             </Head> 
                 <div className='m-3 flex flex-col items-center justify-center min-h-screen text-3xl font-light text-center'>
                 <h1 className='font-bold text-gray-500'>Tudo certo!</h1>
@@ -20,3 +20,6 @@ function index() {
 }
 
 export default index;
+
+
+//            <meta http-equiv="refresh" content="2;https://api.whatsapp.com/send?phone=5563992072188&text=Ola%2C%20gostaria%20de%20realizar%20a%20compra%20do%20meu%20NEW%20HAIR!" />
