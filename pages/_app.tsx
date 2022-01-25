d153642d449f0da075f4d3ce2707658479e635ac
all3x <estudar0101@gmail.com>
2022-01-25 01:53:33 -0300

    now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,8 @@
 import 'tailwindcss/tailwind.css'
 
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
 
-import type { AppProps } from "next/app";
-import { TrackingHeadScript } from "@phntms/next-gtm";
 
-const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || "";
-
-const App = ({ Component }: AppProps) => (
-  <>
-    <TrackingHeadScript id={GA_TRACKING_ID} />
-    <Component />
-  </>
-);
-
-export default App;
+export default MyApp
