3eaf1c0dbee68d0073b749536ae526a604b365a5
all3x <estudar0101@gmail.com>
2022-01-28 22:37:01 -0300

    anality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,7 @@
 import 'tailwindcss/tailwind.css'
 
-import { useRouter } from 'next/router'
-import * as ga from '../lib/ga'
-import { useEffect } from 'react';
-import TagManager from 'react-gtm-module';
-
-
-
-
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
-  useEffect(() => {
-      TagManager.initialize({ gtmId: 'GTM-TQ7LNMH' });
-      
-      
-      const handleRouteChange = (url) => {
-        ga.pageview(url)
-      }
-      router.events.on('routeChangeComplete', handleRouteChange)
-      
-
-      return () => {
-        router.events.off('routeChangeComplete', handleRouteChange)
-      }
-
-    }, [router.events]);
-  return <Component {...pageProps}/>
-  
+  return <Component {...pageProps} />
 }
-
 
 export default MyApp
