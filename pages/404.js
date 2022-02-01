7fc12442082ada5e9bc643c1c5a0f6a0ead39958
all3x <estudar0101@gmail.com>
2022-02-01 00:12:40 -0300

    new page

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,4 @@
 // pages/404.js
 export default function Custom404() {
-    return <h1 className="flex justify-center items-center mt-[-20%] h-screen font-bold text-2xl">404 - Página não encontrada🙄</h1>
+    return <h1 className="flex justify-center items-center mt-[-20%] h-screen font-bold text-xl">404 - Página não encontrada🙄</h1>
   }
