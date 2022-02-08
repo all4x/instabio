4fb6a41cad50f5690cab5fea6014b48deecc5541
all4x <estudar0101@gmail.com>
2022-02-07 23:24:57 -0300

    mudei abobrinha

--- pages/newhair.tsx
+++ pages/newhair.tsx
@@ -2,10 +2,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Newhair from '../public/newhair.jpg'
 import React from 'react';
+import Footer from '../components/Footer'
+import Topbar from '../components/Topbar'
+
+
 
 function newhair() {
   return <div>
-     
+      <Topbar/>
       <div className='m-6 max-w-full lg:ml-auto flex justify-center'>
       <Image src={Newhair} height={400} width={400}/>
       
@@ -15,9 +19,8 @@ function newhair() {
      
       <div className='mx-5 text-center font-sans text-xl text-black '>
                 <div className='xl:mx-6 '>
-                <h2 className='text-3xl p-2 text-gray-900'>O blend de vitaminas número 1º do Brasil.</h2>
-                <p className=' py-2 text-xl text-left text-gray-900'>Composto por minerais, aminoácidos, vitaminas e antioxidantes, o New Hair combate a queda capilar em até 96%.
-                Além disso promove um crescimento acelerado dos fios em até 6x mais rápido e renova cabelo pele e unhas.
+                <h2 className='text-3xl pb-4 text-gray-900'>O blend de vitaminas número 1º do Brasil.</h2>
+                <p className=' py-2 text-xl text-left text-gray-900'> Diminui a queda em 96%, Promove um crescimento acelerado dos fios em até 6x mais rápido e renova cabelo pele e unhas.
                 Com apenas uma cápsula ao dia, é possível notar resultados incríveis na hidratação, brilho e comprimento dos seus fios.</p>
 
 
@@ -32,12 +35,12 @@ function newhair() {
             </div>
           
             <div className='text-center mt-5 p-4'>
-          <Link href="./queda/whatsapp/button-2">
+          <Link href="../pages/blog/queda/whatsapp/button-2">
           <button  className="p-2 pl-16 pr-16 bg-green-500 text-gray-100 text-lg rounded-lg focus:border-4 border-green-300">Whatsapp</button>
           </Link>
       </div> 
       <p className='text-center text-gray-600  mx-6'>frete gratis para toda Araguaína em até 2 dia útil</p>
-   
+   <Footer/>
   </div>;
 }
 
