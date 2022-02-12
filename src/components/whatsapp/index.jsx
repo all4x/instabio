f6ca56f127002140e5b4c57f613cfeb6ef243712
all4x <estudar0101@gmail.com>
2022-02-12 11:51:11 -0300

    button fixad zapzap

--- src/components/whatsapp/index.jsx
+++ src/components/whatsapp/index.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Fab, } from '@mui/material';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import {  green, } from '@mui/material/colors';
+import {  brown, green, } from '@mui/material/colors';
 import { Box } from '@mui/material';
 
 const root = {
@@ -9,7 +9,7 @@ const root = {
 }
 
 const icon = {
-  sx : green
+  sx : brown
 }
 
 // Button of ZapZap
@@ -23,11 +23,11 @@ function App() {
       right={5}
       zIndex={100}
       >
-          <h1 className='font-extrabold text-gray-50 px-2 mx-2 text-xs p-3 bg-green-900 rounded-br-xl'>Como posso te ajudar?</h1>
+          <h1 className='font-extrabold text-white px-2 mx-2 text-xs p-3 bg-green-900 rounded-br-xl'>Como posso te ajudar?</h1>
 
       <a href='https://api.whatsapp.com/send?phone=5563992072188'>
       <Fab color="success" aria-label="edit" size='large'>
-      <WhatsAppIcon sx={{ color: green[50] }} fontSize='large' />
+      <WhatsAppIcon sx={{ color: brown[50] }} fontSize='large' />
       </Fab>
       </a>
       </Box>
