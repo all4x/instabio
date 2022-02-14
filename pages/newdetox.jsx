5751cd6b75d0418154924d6d537935c490c8334c
all4x <estudar0101@gmail.com>
2022-02-14 16:06:47 -0300

    falta 2 kit

--- pages/newdetox.jsx
+++ pages/newdetox.jsx
@@ -10,6 +10,13 @@ import Dois from '../public/2.png'
 import Treis from '../public/3.png'
 import Zap from '../src/components/whatsapp'
 import Topbar from '../src/components/Topbar'
+import Checkout from '../src/components/checkout'
+import Kit2 from '../public/mockup-2.png'
+import Kit3 from '../public/mockup-3.png'
+import Kit5 from '../public/mockup-5.png'
+import Frete from '../public/Frete-gratis.png'
+
+
 
 
 
@@ -31,11 +38,11 @@ export default function Home() {
             <Image src={Potes} width={341} height={341} />
         </div>
         <div className='flex justify-center mb-5'>
-            <Link href="https://nyldecosmeticos.lojaintegrada.com.br/new-detox">
+            <a href="https://app.monetizze.com.br/r/AFN18833790">
                 <Button variant="contained" disableElevation color="success" className='animate-pulse' >
                 Conheça o New detox
                 </Button>
-            </Link>
+            </a>
         </div>
         <div className='bg-green-700'>
 
@@ -75,7 +82,52 @@ export default function Home() {
                 </h1>
         </div>
         </div>
+        <Checkout/>
 
+               {/* CHECKOUT 2 potes*/}
+      <div>
+        <div className="mt-10 mb-5 ml-8 mr-8 flex justify-center bg-gray-100 rounded-xl ">
+          <div className="m-4 p-2 rounded-xl">
+            <div className="text-center">
+              <h1 className="text-2xl font-semibold">Compre 2 Pote</h1>
+              <h2 className="text-xl font bg-gray-500 text-white px-4 rounded-md">
+                Tratamento para 1 Mês
+              </h2>
+            </div>
+            <hr className="bg-yellow-700 m-3 p-[0.2%]" />
+
+            <div className='flex justify-items-start'>
+              <div> <Image src={Kit2}/></div>
+              <div className='text-center flex-col justify-center'>
+                <h1 className='m-2 font-semibold text-gray-600 '>De <strong className='line-through'>R$205,90</strong></h1>
+                <h2 className='text-gray-600'>Por 12x <strong className=' text-black text-3xl font-black'>15,78</strong></h2>
+                <p className='text-gray-600 font-extrabold'>ou R$157,00  à vista</p>
+              </div>
+            </div>
+            <hr className="bg-yellow-700 m-3 p-[0.2%]" />
+            <div className='flex justify-center '>
+            <a href='https://app.monetizze.com.br/r/AFN18833790?u=c&pl=HM142996'>
+            <Button variant="contained" color="success">
+            Comprar
+            </Button>
+            </a>
+            </div>
+          </div>
+          
+        </div>
+        <div className="flex justify-center mb-5">
+        <Link href="/whatsapp">
+          <Button
+            variant="contained"
+            disableElevation
+            color="success"
+            className="animate-pulse"
+          >
+            Comprar Pelo Whatsapp
+          </Button>
+        </Link>
+      </div>
+      </div>
         
         <Footer/>
         </div>
