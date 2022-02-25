c955fce5ebf24d92750b2609925f17279642577c
all4x <estudar0101@gmail.com>
2022-02-25 16:06:10 -0300

    new h2 text-2xl subline

--- pages/newdetox-original.jsx
+++ pages/newdetox-original.jsx
@@ -90,7 +90,7 @@ export default function Home() {
          {/* CHECKOUT INTEIRO */}
       <div className="bg-gray-50 p-5">
       <h1 className='text-center text-4xl font-bold '>Escolha a Melhor Oferta Para Você</h1>
-      <h2 className='text-center font-sans text-3xl'> Perda de 6 a 14kgs com 1 mês de tratamento</h2>
+      <h2 className='text-center font-sans text-2xl'>Elimine de 4 a 6 kilos com 1 mês de tratamento</h2>
       
       </div>
 
