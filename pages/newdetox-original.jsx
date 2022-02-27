89d8cc4ed35e3bda4b7b6b5945d561ba738c2645
all4x <estudar0101@gmail.com>
2022-02-27 15:54:57 -0300

    newpage

--- pages/newdetox-original.jsx
+++ pages/newdetox-original.jsx
@@ -35,7 +35,19 @@ export default function Home() {
         <meta name="description" content="New Detox possui fórmula 100% natural que contém ingredientes potentes capazes de agilizar o processo de emagrecimento, sem precisar sair de casa para academia"/>
         </Head>
         <Zap/>
-        <div className='flex justify-center m-5'>
+
+        {/* TOPBAR HEADLINE*/}
+        <div className='xl:hidden'>
+          <h1 className='text-center font-light text-2xl text-green-800 mt-5 p-2'>Emagreça muito mais rapido</h1>
+          <h2 className='text-center font-extrabold text-3xl text-green-800 my-5'>SEM DIETAS MALUCAS OU EXERCÍCIOS FÍSICOS</h2>
+          <h1 className='text-center font-light text-2xl text-green-800 mt-5 p-2'> CHEGA DE PASSAR FOME E FAZER DIETAS QUE NO FINAL SÓ TE FAZEM ENGORDAR AINDA MAIS!</h1>
+        <div className='m-4 bg-gradient-to-l from-green-800 to-yellow-600 px-2 rounded-lg'>
+        <p className='text-center bg-gray-50 shadow-2xl m-3 px-2 font-sans text-2xl text-gray-800'> New Detox é um poderoso suplemento que acelera o seu metabolismo, diminui o inchaço e a retenção dos líquidos do seu corpo, elimina as substâncias inflamatórias e te faz emagrecer de forma rápida, saudável e definitiva.</p>
+
+        </div>
+        </div>
+
+        <div className='flex justify-center m-5 pt-5'>
             <Image src={Logo} width={317} height={45} />
         </div>
 
