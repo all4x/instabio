aa7b3754ffa691d1fd79adbd7c88b25f8cd61821
all4x <estudar0101@gmail.com>
2022-03-06 17:57:10 -0800

    alo

--- pages/newdetox-original.jsx
+++ pages/newdetox-original.jsx
@@ -25,6 +25,7 @@ import Prova5 from '../public/print1.png'
 import Prova6 from '../public/print2.png'
 
 
+//<Zap/>
 
 
 export default function Home() {
@@ -35,7 +36,6 @@ export default function Home() {
         <link rel="icon" href="/detox.png" />
         <meta name="description" content="New Detox possui fórmula 100% natural que contém ingredientes potentes capazes de agilizar o processo de emagrecimento, sem precisar sair de casa para academia"/>
         </Head>
-        <Zap/>
 
 
         {/* TOPBAR HEADLINE*/}
