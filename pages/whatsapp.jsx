45cf6c89fc99c2877e6e78e36715a29ed6a579a8
Alex Rodrigues <54729143+all4x@users.noreply.github.com>
2022-03-16 22:53:47 -0300

    Update whatsapp.jsx

--- pages/whatsapp.jsx
+++ pages/whatsapp.jsx
@@ -2,23 +2,40 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react';
+import Topbar from '../components/Topbar'
+import Footer from '../components/Footer'
+import Button from '@mui/material/Button';
 
-import Iphone from '/public/Iphone-spinner-2.gif'
+import Logo from '/public/boneca 100x100.png'
 
 
 
 const convertion = () => {
 return <div>
+        <Topbar/>
         <Head>
-            <meta http-equiv="refresh" content="5;https://api.whatsapp.com/send?phone=5563992072188" />
         </Head>
-                <div class="h-screen bg-white">
-        <div class="flex justify-center items-center h-full">
-        <Image className='h-16 w-16' src={Iphone}></Image>
+        <div className=" bg-white mb-40">
+        
+        <div className=" items-center flex justify-center mt-24 ">
+        <Image className=' h-16 w-16' src={Logo}></Image>
         </div>
-
+        <p className='text-center font-semibold text-2xl mt-4'>Tudo Certo!</p>
+        <p className="text-xl font-sans text-center mt-1 p-4"> click no botão abaixo para ser direcionada para o whatsapp</p>
+        <div className="flex justify-center">
+        <a href='https://api.whatsapp.com/send?phone=5563992072188'>
+        <Button variant="contained" color="success">
+            Whatsapp
+            </Button>
+            </a>
         </div>
+        
+        </div>
+        <Footer/>
         </div>;
 };
 
 export default convertion;
+
+
+//             <meta http-equiv="refresh" content="0;https://api.whatsapp.com/send?phone=5563992072188" />
