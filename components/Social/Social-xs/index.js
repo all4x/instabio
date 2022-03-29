14e9cce4d38db2bc6b31d1bcfb23bb0475a8950d
all4x <estudar0101@gmail.com>
2022-03-29 16:34:31 -0300

    new color button social media

--- components/Social/Social-xs/index.js
+++ components/Social/Social-xs/index.js
@@ -11,7 +11,7 @@ function Social() {
 
                 <div className=" text-4xl bg-opacity-25 bg-white text-black rounded-full p-2">
                 <div  className="text-center">
-                    <a href="https://loja.nyldecosmeticos.com.br/"><button class="bg-yellow-300  hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-xl w-80">Loja Online</button></a>
+                    <a href="https://loja.nyldecosmeticos.com.br/"><button class="bg-yellow-200  hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-xl w-80">Loja Online</button></a>
 
             </div>
                 </div>
@@ -20,7 +20,7 @@ function Social() {
             {/*whatsapp*/}
             <div className=" text-4xl bg-opacity-25 bg-white text-black rounded-xl p-2">
                 <div  className="text-center">
-                    <Link href={"whatsapp"} ><button class="bg-yellow-300   hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-xl w-80">Whatsapp</button></Link>
+                    <Link href={"whatsapp"} ><button class="bg-yellow-200   hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-xl w-80">Whatsapp</button></Link>
             </div>
                 </div>
 
@@ -29,7 +29,7 @@ function Social() {
                 <div className=" text-4xl bg-opacity-25 bg-white text-black rounded-full p-2">
                 <div  className="text-center">
                     <Link href="newdetox-original">
-                    <a><button class="bg-yellow-300   hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-xl w-80">New Detox</button></a>
+                    <a><button class="bg-yellow-200   hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-xl w-80">New Detox</button></a>
                     </Link>
             </div>
                 </div>
@@ -39,7 +39,7 @@ function Social() {
 
                  <div className=" text-4xl bg-opacity-25 bg-white text-black rounded-xl p-2">
                 <div  className="text-center">
-                    <a href="https://goo.gl/maps/XyfKMPcYKnTSJG959"><button class="bg-yellow-300  hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-xl w-80">Localização</button></a>
+                    <a href="https://goo.gl/maps/XyfKMPcYKnTSJG959"><button class="bg-yellow-200  hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-xl w-80">Localização</button></a>
             </div>
                 </div>
 
