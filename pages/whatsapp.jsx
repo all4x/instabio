78b9e50a65aeb64e5594a179909a1a7161fd9649
all4x <estudar0101@gmail.com>
2022-05-16 19:40:16 -0300

    new page on zap

--- pages/whatsapp.jsx
+++ pages/whatsapp.jsx
@@ -20,7 +20,7 @@ return <div>
         <div className=" bg-white mb-40">
         
         <div className=" items-center flex justify-center mt-5 ">
-        <Image  src={Logo} height={70} width={70}></Image>
+        <Image  src={Logo} height={60} width={60}></Image>
         </div>
         <p className='text-center font-semibold text-2xl mt-4'>Tudo Certo!</p>
         <p className="text-xl font-sans text-center mt-1 p-4"> click no botão abaixo para ser direcionada para o whatsapp</p>
@@ -31,24 +31,7 @@ return <div>
             </Button>
             </a>
         </div>
-        <hr className="m-5"/>
-
-        <div className='flex justify-center m-4'>
-            <Image src={Foto1} height={250} width={320} className="rounded-2xl"/>
-
-        </div>
-        <h1 className='text-center text-xl m-4 mt-6 font-bold'>Saiba como funciona a Terapia Capilar</h1>
-        <div className="flex justify-center">
-            <Link href={'/blog/terapia'}>
-            <Button variant='contained' color="warning"> 
-        Como Funciona
-        </Button>
-            
-            </Link>
-       
-        </div>
-        
-        
+        <hr className="m-5"/>  
         </div>
         <Footer/>
         </div>;
